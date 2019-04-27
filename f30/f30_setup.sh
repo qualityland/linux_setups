22bc0abe0928e38979d0c276a2ba459b7968db41
Stefan Schmidt <stefan7schmidt@gmx.net>
2019-04-27 22:47:19 +0200

    fedora 30 updated

--- f30/f30_setup.sh
+++ f30/f30_setup.sh
@@ -1,17 +1,16 @@
 #! /usr/bin/env bash
 
 # f30_setup.sh
-# Setup a Fedora 30 Workstation
 #
-# 2019-03-05 SJS
-# adds:	        - rpmfusion repository
-#               - Sublime Text 3 repository
-# installs:     - R & RStudio
-#               - libs (to build common R packages)
-#               - Tools (MySQL client, htop, zsh, tmux, vlc...) 
-#               - LaTeX (texlive)
-#
-# 2019-04-16 SJS added qBitttorent, Gnome Tweaks, Pygame
+# 2019-04-27 SJS
+# installs	- rpmfusion repositories
+#		- Sublime Text 3
+#		- R & RStudio
+#		- libs (to build common R packages)
+#		- Tools (MySQL client, htop, zsh, tmux, vlc, gnote, rednotebook) 
+#		- LaTeX (texlive)
+#		- sschmidt: create RStudio directory and .gitconfig
+
 
 # rpm fusion
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -23,7 +22,7 @@ sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x
 sudo dnf update
 sudo dnf install -y binutils gcc make patch glibc-headers glibc-devel kernel-headers kernel-devel dkms vim subversion gcc-c++
 sudo dnf install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel
-sudo dnf install -y R vlc sublime-text tmux htop zsh gnome-todo python3-csvkit f30-backgrounds-extras-gnome qbittorrent gnome-tweaks python3-pygame # mariadb focuswriter okular
+sudo dnf install -y R vlc sublime-text tmux htop zsh gnome-todo python3-csvkit python3-pygame f30-backgrounds-extras-gnome gnome-tweaks qbittorrent gnote rednotebook # mariadb focuswriter okular
 
 # rstudio
 cd /home/sschmidt/Downloads/R/
@@ -32,5 +31,19 @@ sudo dnf install -y rstudio-1.1.463-x86_64.rpm
 
 # texlive
 #sudo dnf install -y texlive-scheme-full
+
+# user environment
+runuser sschmidt -c "mkdir -p /home/sschmidt/wrk/studio /home/sschmidt/Documents/eBooks"
+# eBooks
+cd /run/media/sschmidt/KINGSTON/Documents/
+runuser sschmidt -c "cp -r * /home/sschmidt/Documents/eBooks"
+# R scripts
+cd /run/media/sschmidt/KINGSTON/Downloads/
+runuser sschmidt -c "cp -r cd_keys.txt R /home/sschmidt/Downloads/"
+# git config
+runuser sschmidt -c "git config --global user.name 'Stefan Schmidt'"
+runuser sschmidt -c "git config --global user.email 'stefan7schmidt@gmx.net'"
+
+
 
 
