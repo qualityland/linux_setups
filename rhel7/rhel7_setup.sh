b8734e1a97daf328ef68ba7ec23de8b20459098a
Stefan Schmidt <stefan7schmidt@gmx.net>
2019-05-14 12:58:32 +0200

    rhel7: removed old scripts.

--- rhel7/rhel7_setup.sh
+++ rhel7/rhel7_setup.sh
@@ -3,32 +3,35 @@
 # rhel7_setup.sh
 #
 # 2019-05-14 SJS
+# has to be run as root or sudo.
+#
 # installs	- epel, rpmfusion and sublime repositories
 #		- R & RStudio
+#		- python3
 #		- libs (to build common R packages)
 #		- Tools (MySQL client, htop, zsh, tmux, vlc...) 
 
 # add epel & rpm fusion repo
-sudo yum install -y epel-release
-sudo yum install -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
+yum install -y epel-release
+yum install -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 
 # add sublime repo
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 # install packages
-sudo yum update
-sudo yum install -y gcc patch glibc-headers glibc-devel kernel-headers kernel-devel dkms gcc-c++ tree git # subversion
-sudo yum install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel
-sudo yum install -y R tmux mariadb htop zsh vlc sublime-text # rednotebook focuswriter okular
+yum update
+yum install -y gcc patch glibc-headers glibc-devel kernel-headers kernel-devel dkms gcc-c++ tree git # subversion
+yum install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel
+yum install -y R tmux mariadb htop zsh vlc sublime-text # rednotebook focuswriter okular
 
 # rstudio
 cd ~/Downloads/
 wget https://download1.rstudio.org/desktop/centos7/x86_64/rstudio-1.2.1335-x86_64.rpm
-sudo yum install -y rstudio-1.2.1335-x86_64.rpm
+yum install -y rstudio-1.2.1335-x86_64.rpm
 
 # texlive
-sudo dnf install -y texlive-scheme-full
+dnf install -y texlive
 
 
 # python3
