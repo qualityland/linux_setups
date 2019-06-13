330fb527f65bd091c1bf54bbdf96550a003aac85
Stefan Schmidt <stefan7schmidt@gmx.net>
2019-06-13 10:45:49 +0200

    f30 and u19.04 consolidated.

--- f30/f30_setup.sh
+++ f30/f30_setup.sh
@@ -10,44 +10,52 @@
 #		- Tools (MySQL client, htop, zsh, tmux, vlc, gnote, rednotebook) 
 #		- LaTeX (texlive)
 #		- sschmidt: create RStudio directory and .gitconfig
+#
+# 2019-06-11 SJS install RStudio 1.2
 
+
+# variables
+MY_USER="sschmidt"
+MY_HOME="/home/$MY_USER"
+USB_STICK="/run/media/$MY_USER/Transcend/lx"
 
 # rpm fusion
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # sublime
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
-sudo dnf update -y
-sudo dnf install -y binutils gcc make patch glibc-headers glibc-devel kernel-headers kernel-devel dkms vim subversion gcc-c++
-sudo dnf install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel exfat-utils fuse-exfat
-sudo dnf install -y R vlc sublime-text tmux htop zsh gnome-todo python3-csvkit python3-pip f30-backgrounds-extras-gnome gnome-tweaks qbittorrent gnote rednotebook # mariadb focuswriter okular
+dnf update -y
+dnf install -y binutils gcc make patch glibc-headers glibc-devel kernel-headers kernel-devel dkms vim gcc-c++ # subversion
+dnf install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel exfat-utils fuse-exfat
+dnf install -y R vlc sublime-text tmux htop zsh gnome-todo python3-csvkit python3-pip f30-backgrounds-extras-gnome gnome-tweaks qbittorrent gnote rednotebook # mariadb focuswriter okular
 
 # user environment
 # eBooks
-cd /run/media/sschmidt/KINGSTON/Documents/
-runuser sschmidt -c "cp -r eBooks /home/sschmidt/Documents/"
-
+runuser $MY_USER -c "cp -r --no-preserve=all $USB_STICK/Documents/* $MY_HOME/Documents/"
 # Pictures
-cd /run/media/sschmidt/KINGSTON/Pictures/
-runuser sschmidt -c "cp -r Portrait /home/sschmidt/Pictures/"
-
+runuser $MY_USER -c "cp -r --no-preserve=all $USB_STICK/Pictures/* $MY_HOME/Pictures/"
 # R scripts
-cd /run/media/sschmidt/KINGSTON/Downloads/
-runuser sschmidt -c "cp -r cd_keys.txt R /home/sschmidt/Downloads/"
+runuser $MY_USER -c "cp -r --no-preserve=all $USB_STICK/Downloads/* $MY_HOME/Downloads/"
+# Dirwctories
+runuser $MY_USER -c "cd $MY_HOME; mkdir -p wrk/studio src tmp"
 
 # git config
-runuser sschmidt -c "git config --global user.name 'Stefan Schmidt'"
-runuser sschmidt -c "git config --global user.email 'stefan7schmidt@gmx.net'"
+#runuser sschmidt -c "git config --global user.name 'Stefan Schmidt'"
+#runuser sschmidt -c "git config --global user.email 'stefan7schmidt@gmx.net'"
 
 # rstudio
-cd /home/sschmidt/Downloads/R/
-#wget https://download1.rstudio.org/rstudio-1.1.463-x86_64.rpm
-sudo dnf install -y rstudio-1.1.463-x86_64.rpm
+cd $MY_HOME/Downloads/R/
+#wget https://download1.rstudio.org/rstudio-1.2.1335-x86_64.rpm
+# install old openssl version for rstudio
+# Fedora uses OpenSSL 1.1, but rstudio 1.2.1335 expects 1.0.
+# see: https://community.rstudio.com/t/rstudio-on-fedora-30-error-while-loading-shared-libraries-libssl-so-10/31380
+dnf install -y compat-openssl10
+dnf install -y rstudio-1.2.1335-x86_64.rpm
 
 # texlive
-sudo dnf install -y texlive-scheme-full
+dnf install -y texlive-scheme-full
 
 
 
--- u19.04/u19-04_setup.sh
+++ u19.04/u19-04_setup.sh
@@ -8,31 +8,49 @@
 #
 # 2019-05-13 SJS
 # git and build-essential are already installed per default
+# 2019-06-09 SJS
+# sudo's removed, exfat support added, copying from usb stick added.
+
+
+# variables
+MY_USER="sschmidt"
+MY_HOME="/home/$MY_USER"
+USB_STICK="/media/$MY_USER/Transcend/lx"
 
 # CRAN R repository
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys  51716619E084DAB9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys  51716619E084DAB9
 printf '#CRAN mirror\ndeb https://cran.rstudio.com/bin/linux/ubuntu disco-cran35/\ndeb-src https://cran.rstudio.com/bin/linux/ubuntu disco-cran35/\n' | sudo tee -a /etc/apt/sources.list.d/cran-mirror.list
 
 # Sublime repository
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
 # Rednotebook PPA
-sudo add-apt-repository ppa:rednotebook/stable -y
+add-apt-repository ppa:rednotebook/stable -y
 
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
 # some development tools and libraries
-sudo apt-get install -y libssl-dev libxml2-dev libcurl4-openssl-dev libmariadbclient-dev libxt-dev libopenblas-dev python3-pygame python3-csvkit exfat-fuse exfat-utils
-sudo apt-get install -y vim tree htop tmux zsh gdebi apt-transport-https r-base mariadb-client sublime-text texlive-full calibre gnome-todo qbittorrent
+apt-get install -y libssl-dev libxml2-dev libcurl4-openssl-dev libmariadbclient-dev libxt-dev libopenblas-dev exfat-fuse exfat-utils
+apt-get install -y vim tree htop tmux zsh gdebi git apt-transport-https r-base mariadb-client sublime-text texlive-full calibre gnome-todo qbittorrent
 
-# RStudio
-cd ~/Downloads/R/
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1335-amd64.deb
-sudo gdebi -n rstudio-1.2.1335-amd64.deb
-printf '\nexport QT_STYLE_OVERRIDE=gtk\n' | sudo tee -a ~/.profile
+# user environment
+# eBooks
+runuser $MY_USER -c "cp -r --no-preserve=all $USB_STICK/Documents/* $MY_HOME/Documents/"
+
+# Pictures
+runuser $MY_USER -c "cp -r --no-preserve=all $USB_STICK/Pictures/* $MY_HOME/Pictures/"
+
+# R scripts
+runuser $MY_USER -c "cp -r --no-preserve=all $USB_STICK/Downloads/* $MY_HOME/Downloads/"
 
 # git config
-runuser sschmidt -c "git config --global user.name 'Stefan Schmidt'"
-runuser sschmidt -c "git config --global user.email 'stefan7schmidt@gmx.net'"
+runuser $MY_USER -c  "git config --global user.name 'Stefan Schmidt'"
+runuser $MY_USER -c  "git config --global user.email 'stefan7schmidt@gmx.net'"
 
+# rstudio
+cd $MY_HOME/Downloads/R/
+gdebi -n rstudio-1.2.1335-amd64.deb
+runuser $MY_USER -c "printf '\nexport QT_STYLE_OVERRIDE=gtk\n' | sudo tee -a ~/.profile"
+
+runuser $MY_USER -c "cd $MY_HOME; mkdir -p wrk/studio src tmp"
