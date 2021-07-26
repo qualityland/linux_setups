be37bb985ec08239a11b4e2ed2d3d2a6e0defbea
Stefan Schmidt <sschmidt@pop-os.localdomain>
2021-07-26 13:02:05 +0200

    pop-os 21.04 added

---
+++ pop-os_21.04/setup.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+
+# setup.sh
+# installs an R development environment
+# on an Pop!_OS 21.04 machine.
+
+# 2020-12-19 SJS
+# installs:
+#       - dev tools & libs
+#	- R & RStudio
+#	- Sublime Text 3
+#	- Rednotebook, Todo, Calibre, Texlive
+#
+
+# CRAN R repository
+
+# add the signing key (by Michael Rutter) for these repos
+# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+# Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+#add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
+
+# Sublime repository
+#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+#echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+
+# Rednotebook PPA
+#add-apt-repository ppa:rednotebook/stable -y
+
+apt update && apt upgrade -y
+
+# some development tools and libraries
+apt install -y build-essential libssl-dev libxml2-dev libcurl4-openssl-dev libxt-dev libopenblas-dev
+#apt install -y exfat-fuse exfat-utils abcde cd-discid lame cdparanoia id3 id3v2 eyed3
+apt install -y vim tree htop tmux zsh gdebi git apt-transport-https r-base mariadb-client texlive-full calibre gnome-todo qbittorrent # sublime-text
+
+# rstudio
+#cd /tmp/
+#wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1717-amd64.deb
+#gdebi -n rstudio-1.4.1717-amd64.deb
+
