fc8f2c61cce461db8ff7de72f15212d9ef67a664
Stefan Schmidt <sschmidt@kauai.example.com>
2021-10-06 17:13:44 +0200

    u20.04: rstudio updated.

--- u20.04/setup.sh
+++ u20.04/setup.sh
@@ -47,6 +47,6 @@ apt install -y vim tree htop tmux zsh gdebi git apt-transport-https r-base maria
 
 # rstudio
 cd /tmp/
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1717-amd64.deb
-gdebi -n rstudio-1.4.1717-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.0-351-amd64.deb
+gdebi -n rstudio-2021.09.0-351-amd64.deb
 
