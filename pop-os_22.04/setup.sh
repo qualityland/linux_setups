a3f70e84594635d64855608f4b829400a40b9070
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-07-01 18:07:09 +0200

    pop_os22.04: updated

--- pop-os_22.04/setup.sh
+++ pop-os_22.04/setup.sh
@@ -15,15 +15,15 @@
 # CRAN R repository
 
 # update indices
-sudo apt update -qq
+apt update -qq
 # install two helper packages we need
-sudo apt install --no-install-recommends software-properties-common dirmngr
+apt install --no-install-recommends software-properties-common dirmngr
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
 # Sublime repository
 #wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
@@ -42,7 +42,7 @@ apt install -y jags libzmq3-dev libnode-dev libfontconfig-dev default-jre defaul
 apt install -y vim tree htop tmux zsh gdebi apt-transport-https r-base mariadb-client texlive-full calibre gnome-todo qbittorrent # sublime-text
 
 # rstudio
-#cd /tmp/
-#wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1717-amd64.deb
-#gdebi -n rstudio-1.4.1717-amd64.deb
+cd /tmp/
+wget https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.02.3-492-amd64.deb
+gdebi -n rstudio-2022.02.3-492-amd64.deb
 
