145a625f87eeb1bc9661f5f6cf5fb0b01c96c1f8
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-07-07 10:02:38 +0200

    rstudio updated

--- rhel8/setup.sh
+++ rhel8/setup.sh
@@ -41,8 +41,8 @@ dnf install -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.
 #dnf update -y
 dnf install -y exfat-utils fuse-exfat
 dnf group install -y 'Development Tools'
-dnf install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel pam-devel rdma-core-devel libibverbs-utils readline-devel sqlite-devel libffi-devel
-dnf install -y yum-utils tmux vlc R mariadb zsh # gnome-tweaks sublime-text
+dnf install -y openssl-devel libcurl-devel libxml2-devel libjpeg-turbo-devel mariadb-devel pam-devel rdma-core-devel libibverbs-utils readline-devel sqlite-devel libffi-devel zeromq-devel
+dnf install -y yum-utils tmux vlc R mariadb zsh foliate # gnome-tweaks sublime-text
 
 # texlive
 #yum install -y texlive
@@ -57,7 +57,7 @@ dnf install -y yum-utils tmux vlc R mariadb zsh # gnome-tweaks sublime-text
 
 
 # rstudio
-dnf install -y https://download1.rstudio.org/desktop/centos8/x86_64/rstudio-2021.09.2-382-x86_64.rpm
+dnf install -y https://download1.rstudio.org/desktop/rhel8/x86_64/rstudio-2022.02.3-492-x86_64.rpm
 
 # not now...maybe later
 #yum install -y focuswriter okular
