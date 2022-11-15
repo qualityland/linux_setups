99f0a18abe6d421aac9dad667a7288c735da0f59
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-11-15 11:16:38 +0100

    u22.04: rstudio updated, obsidian added.

--- u22.04/setup.sh
+++ u22.04/setup.sh
@@ -8,7 +8,7 @@
 # installs:
 #       - dev tools & libs
 #       - R & RStudio
-#       - Sublime Text 3
+#       - Obsidian
 #       - Rednotebook, Todo, Calibre, Texlive
 #
 
@@ -16,18 +16,12 @@
 
 # update indices
 apt update -qq
-# install two helper packages we need
-#apt install --no-install-recommends software-properties-common dirmngr
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
 add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-
-# Sublime repository
-#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-#echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
 # Rednotebook PPA
 #add-apt-repository ppa:rednotebook/stable -y
@@ -36,12 +30,17 @@ add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_relea
 
 # some development tools and libraries
 apt install -y build-essential libssl-dev libxml2-dev libcurl4-openssl-dev libxt-dev libopenblas-dev
+
 # some libraries needed to install contributed R packages
-apt install -y jags libzmq3-dev libnode-dev libfontconfig1-dev default-jre default-jdk libmagick++-dev cmake libglpk-dev libmpfr-dev libcairo2-dev libgsl-dev
+apt install -y jags libzmq3-dev libnode-dev libfontconfig-dev default-jre default-jdk libmagick++-dev cmake libglpk-dev libmpfr-dev libcairo2-dev libgsl-dev r-bioc-rbgl libharfbuzz-dev libfribidi-dev
 #apt install -y exfat-fuse exfat-utils abcde cd-discid lame cdparanoia id3 id3v2 eyed3
-apt install -y vim tree htop tmux zsh gdebi git apt-transport-https r-base mariadb-client texlive-full calibre qbittorrent # sublime-text
+apt install -y vim tree htop tmux zsh gdebi git apt-transport-https r-base mariadb-client texlive-full calibre qbittorrent rednotebook texstudio
 
 # rstudio
 cd /tmp/
-wget https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.02.3-492-amd64.deb
-gdebi -n rstudio-2022.02.3-492-amd64.deb
+wget https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.2-576-amd64.deb
+gdebi -n rstudio-2022.07.2-576-amd64.deb
+
+# obsidian
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.0.3/obsidian_1.0.3_amd64.deb
+gdebi -n obsidian_1.0.3_amd64.deb
