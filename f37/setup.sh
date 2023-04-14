e64f434e515cc63601321c6c8f305e98c0ddac14
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2023-04-14 12:44:21 +0200

    f37: rpmfusion, codecs and flathub added

--- f37/setup.sh
+++ f37/setup.sh
@@ -1,23 +1,46 @@
 #! /usr/bin/env bash
 
 # setup.sh
-#
-# 2022-09-02 SJS
-# installs:
-#		- R & RStudio
-#		- libs (to build common R packages)
-#		- Tools (MySQL client, zsh, tmux, rednotebook) 
-#		- LaTeX (texlive)
+
+# does:
+# - updates installed packages
+# - adds rpmfusion repositories
+# - adds flathub repostiory
+# - adds multimedia codecs
+
+# optionaly:
+# - R & RStudio
+# - libs (to build common R packages)
+# - Tools (MySQL client, zsh, tmux, rednotebook, PostgreSQL) 
+# - LaTeX (texlive)
 
 
 
+# DNF Configuration
+## for speed add to /etc/dnf/dnf.conf:
+# fastestmirror=True
+# max_parallel_downloads=10
+# defaultyes=True
+# keepcache=True
 
-# Sublime repo
-#rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-#dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
-
+# Update installed Packages
 dnf update -y
+
+
+# Add RPM Fusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+# Add Flathub
+flatpak remote-add --if-not-exists https://flathub.org/repo/flathub.flatpakrepo
+
+
+# Add Media Codecs
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf groupupdate sound-and-video
+
+
 # development tools
 dnf install -y binutils gcc make patch glibc-headers glibc-devel kernel-headers kernel-devel dkms vim gcc-c++ tmux zsh Lmod
 
@@ -44,6 +67,12 @@ dnf install -y texlive-scheme-full
 # musescore
 #dnf copr enable jjames/MuseScore
 #dnf install -y musescore
+
+
+# Sublime repo
+#rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+#dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
 
 # MacBook Pro Wifi
 # rpm fusion repo
