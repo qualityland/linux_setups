7ff8d7371167df8125be8288db347cf7f111691a
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2023-04-19 11:26:13 +0200

    f38 updated

--- f38/setup.sh
+++ f38/setup.sh
@@ -1,24 +1,46 @@
 #! /usr/bin/env bash
 
 # setup.sh
-#
-# 2023-04-08 SJS
-# installs:
-#		- R & RStudio
-#		- libs (to build common R packages)
-#		- Tools (MySQL client, zsh, tmux, rednotebook) 
-#		- LaTeX (texlive)
-#               - PostgreSQL & pgAdmin4
+
+# does:
+# - updates installed packages
+# - adds rpmfusion repositories
+# - adds flathub repostiory
+# - adds multimedia codecs
+
+# optionaly:
+# - R & RStudio
+# - libs (to build common R packages)
+# - Tools (MySQL client, zsh, tmux, rednotebook, PostgreSQL) 
+# - LaTeX (texlive)
 
 
 
+# DNF Configuration
+## for speed add to /etc/dnf/dnf.conf:
+# fastestmirror=True
+# max_parallel_downloads=10
+# defaultyes=True
+# keepcache=True
 
-# Sublime repo
-#rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-#dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
-
+# Update installed Packages
 dnf update -y
+
+
+# Add RPM Fusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+# Add Flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+# Add Media Codecs
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf groupupdate sound-and-video
+
+
 # development tools
 dnf install -y binutils gcc make patch glibc-headers glibc-devel kernel-headers kernel-devel dkms vim gcc-c++ tmux zsh Lmod
 
@@ -36,7 +58,7 @@ dnf install -y libcurl-devel libgit2-devel libibverbs-utils libtiff-devel libjpe
 dnf install -y R rstudio-desktop # python3-csvkit sublime-text
 
 # desktop apps
-dnf install -y rednotebook foliate f38-backgrounds-extras-gnome # gtksourceview3
+dnf install -y rednotebook foliate f37-backgrounds-extras-gnome # gtksourceview3
 
 # texlive
 dnf install -y texlive-scheme-full
@@ -45,6 +67,12 @@ dnf install -y texlive-scheme-full
 dnf install -y postgresql-server postgresql-contrib postgis postgresql-plpython3
 postgresql-setup --initdb --unit postgresql
 systemctl enable postgresql
+# edit /var/lib/pgsql/data/pg_hba.conf
+# replace line:
+# vhost    all             all             127.0.0.1/32            ident
+# with:
+# host    all             all             127.0.0.1/32            md5
+systemctl start postgresql
 
 # pgAdmin4 (PostgreSQL Admin Tool)
 dnf install -y https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedora-repo-2-1.noarch.rpm
@@ -54,19 +82,22 @@ dnf install -y https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedo
 # with:
 # baseurl=https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/fedora/fedora-37-$basearch
 dnf install -y pgadmin4-desktop
-# edit /var/lib/pgsql/data/pg_hba.conf
-# replace line:
-# vhost    all             all             127.0.0.1/32            ident
-# with:
-# host    all             all             127.0.0.1/32            md5
-systemctl start postgresql
+# connect to DB and set password for user postgres
+# sudo -u postgres psql postgres
+# \password postgres
+# \q
+
+# Sublime repo
+#rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+#dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
 
 # musescore
 #dnf copr enable jjames/MuseScore
 #dnf install -y musescore
 
+
 # MacBook Pro Wifi
 # rpm fusion repo
-#dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 #dnf install -y akmods broadcom-wl
 #akmods
