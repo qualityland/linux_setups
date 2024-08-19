f2f31ba0a1003d09e4c8e15872a8b973577693b8
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2024-08-19 06:32:24 -0400

    deb12: rstudio updated

--- deb12/setup-sudo.sh
+++ deb12/setup-sudo.sh
@@ -47,6 +47,8 @@ apt install -y  r-base
 
 # rstudio
 cd /tmp/
-wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.09.1-494-amd64.deb
-gdebi -n rstudio-2023.09.1-494-amd64.deb
+wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.04.2-764-amd64.deb
+gdebi -n rstudio-2024.04.2-764-amd64.deb
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb
+gdebi -n obsidian_1.6.7_amd64.deb
 
