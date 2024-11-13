c357062985320577b89821a671f87681a46d2c71
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2024-11-13 08:57:28 +0100

    u24.04: rstudio updated

--- u24.04/setup.sh
+++ u24.04/setup.sh
@@ -25,13 +25,14 @@ add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_relea
 # some development tools and libraries
 apt install -y build-essential libssl-dev libxml2-dev libcurl4-openssl-dev libxt-dev libopenblas-dev libgdal-dev libgeos-dev libproj-dev 
 # some libraries needed to install contributed R packages
-apt install -y jags libzmq3-dev libnode-dev libfontconfig1-dev default-jre default-jdk libmagick++-dev cmake libglpk-dev libmpfr-dev libcairo2-dev libgsl-dev libharfbuzz-dev libfribidi-dev r-bioc-rbgl
+apt install -y jags libzmq3-dev libnode-dev libfontconfig1-dev default-jre default-jdk libmagick++-dev cmake libglpk-dev libmpfr-dev
+apt install -y libcairo2-dev libgsl-dev libharfbuzz-dev libfribidi-dev r-bioc-rbgl
 
 # some tools
 apt install -y vim tree htop tmux zsh gdebi git apt-transport-https r-base mariadb-client texlive-full calibre qbittorrent # sublime-text
 
 # rstudio
-#cd /tmp/
-#wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.09.1-394-amd64.deb
-#gdebi -n rstudio-2024.04.0-735-amd64.deb
+cd /tmp/
+wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.09.1-394-amd64.deb
+gdebi -n rstudio-2024.04.0-735-amd64.deb
 
